79dae171683bbf76665324ccb163af8ca775cd3f
Yousif Mubareka <yousifmubarka@gmail.com>
2020-09-19 01:14:24 +0300

    todo app antd ui

---
+++ todo-app/pages/index.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import { Row, Col, Input, Button, Card } from "antd";
+import { PlusOutlined, DeleteOutlined } from "@ant-design/icons";
+
+export default function Home() {
+  const [todoText, setToDoText] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const handleText = (obj) => {
+    setToDoText(obj.target.value);
+  };
+
+  const addTodo = (todo) => {
+    setTodoList([todo, ...todoList]);
+    setToDoText("");
+    localStorage.setItem("todos", JSON.stringify([todo, ...todoList]));
+  };
+
+  const clearList = () => {
+    setTodoList([]);
+    localStorage.setItem("todos", "");
+  };
+
+  const removeItem = (i) => {
+    setTodoList(
+      todoList.filter((el) => {
+        el !== i;
+      })
+    );
+
+    localStorage.setItem(
+      "todos",
+      JSON.stringify(
+        todoList.filter((el) => {
+          el !== i;
+        })
+      )
+    );
+  };
+
+  useEffect(() => {
+    let todos = localStorage.getItem("todos");
+    if (todos) {
+      setTodoList(JSON.parse(todos));
+    }
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="form">
+        <Row gutter={20}>
+          <Col span={17}>
+            <Input placeholder="TODO" value={todoText} onChange={handleText} />
+          </Col>
+          <Col span={4}>
+            <Button
+              style={{ width: "100%" }}
+              onClick={() => {
+                addTodo(todoText);
+              }}
+            >
+              Add
+            </Button>
+          </Col>
+          <Col span={3}>
+            <Button style={{ width: "100%" }} danger onClick={clearList}>
+              Remove all
+            </Button>
+          </Col>
+        </Row>
+      </div>
+      <div class="todos">
+        {todoList.map((a) => (
+          <Card>
+            <Row>
+              <Col span={21}>
+                <h3>{a}</h3>
+              </Col>
+              <Col span={3}>
+                <Button style={{ width: "100%" }} onClick={() => removeItem(a)}>
+                  Remove
+                </Button>
+              </Col>
+            </Row>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
